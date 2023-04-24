1dd2c33edb091ac7c67b42d155b9d0d6c8d69911
root <root@ip-172-31-139-182.ap-south-1.compute.internal>
2023-04-24 15:16:06 +0000

    Made changes to the Install script

--- Install_tools.sh
+++ Install_tools.sh
@@ -17,4 +17,33 @@ systemctl start docker
 echo -e "$msg Getting Kubectl package....\n"
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 
+
+
+echo -e "$msg Make the kubectl binary executable:\n"
+chmod +x ./kubectl
+
+echo -e "$msg Move the binary in to your PATH:\n"
+mv ./kubectl /usr/local/bin/kubectl
+
+echo -e "$msg Check version:\n"
+kubectl version --client
+
+echo -e "$msg Minikube install:\n"
+
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
+&& chmod +x minikube \
+&& sudo mv minikube /usr/local/bin/
+
+minikube version
+
+echo -e "$msg Start minikube with docker driver"
+
+minikube start --driver=docker --force
+
+echo -e "$msg  Check status:"
+minikube status
+kubectl get pods
+kubectl get svc
+
+
 echo -e "$msg END of $0"
